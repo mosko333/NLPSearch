1833a5d51346d7a0b1e8c9714952c53c9fc6fc35
AdamAM <adamam@gmail.com>
2018-08-24 12:01:08 -0600

    Initial Commit

---
+++ EightyThreeCreativeAdamMoskovich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EightyThreeCreativeAdamMoskovich
+//
+//  Created by Adam on 24/08/2018.
+//  Copyright © 2018 Adam Moskovich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
